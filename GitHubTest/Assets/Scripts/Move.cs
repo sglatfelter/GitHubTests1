fec8ddc1c229971f70a087dacc1a5aabba312814
SteveG <sglatfelter@mica.edu>
2019-02-04 11:14:25 -0500

    CharacterMovement
    Collision Detection and Movement

---
+++ GitHubTest/Assets/Scripts/Move.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+    public GameObject Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Movement = Camera.transform.TransformDirection(Movement);
+        rb.AddForce(Movement * speed);
+
+   
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Hit")
+        {
+            print("Ouch");
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
